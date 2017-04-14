bcc5b8b2ef9bf5710d6f29f5f04afd845e934040
MICHAEL JACKSON <mjijackson@gmail.com>
2017-04-14 14:12:58 -0700

    Finish contacts app

--- contacts/src/App.js
+++ contacts/src/App.js
@@ -1,23 +1,22 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import sortBy from 'sort-by'
 import serializeForm from 'form-serialize'
 import escapeRegExp from 'escape-string-regexp'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import * as ContactsAPI from './ContactsAPI'
 import ImageInput from './ImageInput'
 import './App.css'
 
 class ContactsApp extends React.Component {
   state = {
-    contacts: [],
-    query: ''
+    contacts: []
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim() })
-  }
-
-  clearQuery = () => {
-    this.setState({ query: '' })
+  componentDidMount() {
+    ContactsAPI.getAll().then(contacts => {
+      this.setState({ contacts })
+    })
   }
 
   addContact = (contact) => {
@@ -30,34 +29,55 @@ class ContactsApp extends React.Component {
     this.setState(state => ({
       contacts: state.contacts.filter(c => c.id !== contact.id)
     }))
-
-    ContactsAPI.remove(contact)
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault()
-
-    const form = event.target
-    const values = serializeForm(form, { hash: true })
-
-    if (values.name && values.email) {
-      ContactsAPI.create(values).then(contact => {
-        this.addContact(contact)
-        form.reset()
-      })
-    } else {
-      window.alert('Please enter both a name and email address')
-    }
-  }
-
-  componentDidMount() {
-    ContactsAPI.getAll().then(contacts => {
-      this.setState({ contacts })
-    })
   }
 
   render() {
-    const { contacts, query } = this.state
+    const { contacts } = this.state
+
+    return (
+      <BrowserRouter>
+        <div className="app">
+          <Route exact path="/" render={() => (
+            <ListContactsPage contacts={contacts} onDeleteContact={this.removeContact}/>
+          )}/>
+          <Route path="/create" render={({ history }) => (
+            <CreateContactPage onCreateContact={contact => {
+              this.addContact(contact)
+              history.push('/')
+            }}/>
+          )}/>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+class ListContactsPage extends React.Component {
+  static propTypes = {
+    contacts: PropTypes.array.isRequired,
+    onDeleteContact: PropTypes.func.isRequired
+  }
+
+  state = {
+    query: ''
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+
+  handleDelete = (contact) => {
+    ContactsAPI.remove(contact)
+    this.props.onDeleteContact(contact)
+  }
+
+  render() {
+    const { contacts } = this.props
+    const { query } = this.state
 
     let showingContacts
     if (query) {
@@ -70,14 +90,18 @@ class ContactsApp extends React.Component {
     showingContacts.sort(sortBy('name'))
 
     return (
-      <div className="app">
-        <div className="search-contacts">
-          <input
-            type="search"
-            value={query}
-            onChange={event => this.updateQuery(event.target.value)}
-            placeholder="Search contacts"
-          />
+      <div className="list-contacts">
+        <div className="list-contacts-top">
+          <div className="search-contacts">
+            <input
+              type="text"
+              value={query}
+              onChange={event => this.updateQuery(event.target.value)}
+              placeholder="Search contacts"
+            />
+          </div>
+
+          <Link className="add-contact" to="/create">Add a New Contact</Link>
         </div>
 
         {showingContacts.length !== contacts.length && (
@@ -97,24 +121,52 @@ class ContactsApp extends React.Component {
                 <p className="contact-name">{contact.name}</p>
                 <p className="contact-email">{contact.email}</p>
               </div>
-              <div className="remove-contact-button">
-                <button onClick={() => this.removeContact(contact)}>Remove</button>
+              <div className="contact-remove">
+                <button onClick={() => this.handleDelete(contact)}>Remove</button>
               </div>
             </li>
           ))}
         </ol>
+      </div>
+    )
+  }
+}
+
+class CreateContactPage extends React.Component {
+  static propTypes = {
+    onCreateContact: PropTypes.func.isRequired
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const form = event.target
+    const values = serializeForm(form, { hash: true })
+
+    if (values.name && values.email) {
+      ContactsAPI.create(values).then(contact => {
+        this.props.onCreateContact(contact)
+        form.reset()
+      })
+    } else {
+      window.alert('Please enter both a name and email address')
+    }
+  }
+
+  render() {
+    return (
+      <div className="create-contact">
+        <Link className="close-create-contact" to="/">Close</Link>
 
         <form onSubmit={this.handleSubmit}>
           <div className="create-contact-wrapper">
-            <div className="create-contact">
-              <div className="create-contact-avatar">
-                <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64}/>
-              </div>
-              <div className="create-contact-details">
-                <input className="create-contact-name" type="text" name="name" placeholder="Name"/>
-                <input className="create-contact-email" type="text" name="email" placeholder="Email"/>
-              </div>
-              <div className="create-contact-button">
+            <div className="create-contact-avatar">
+              <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64}/>
+            </div>
+            <div className="create-contact-details">
+              <input className="create-contact-name" type="text" name="name" placeholder="Name"/>
+              <input className="create-contact-email" type="text" name="email" placeholder="Email"/>
+              <div className="create-contact-save">
                 <button>Add Contact</button>
               </div>
             </div>
