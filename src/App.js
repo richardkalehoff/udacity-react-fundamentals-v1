5f1714bb5bca2ee13fea97c51450da075c82e7af
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-04-26 15:55:06 -0600

    3-props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    "id":"ryan",
+    "name":"Ryan Florence",
+    "email":"ryan@reacttraining.com",
+    "avatarURL":"http://localhost:5001/ryan.jpg"
+  },
+  {
+    "id":"michael",
+    "name":"Michael Jackson",
+    "email":"michael@reacttraining.com",
+    "avatarURL":"http://localhost:5001/michael.jpg"
+  },
+  {
+    "id":"tyler",
+    "name":"Tyler McGinnis",
+    "email":"tyler@reacttraining.com",
+    "avatarURL":"http://localhost:5001/tyler.jpg"
+  }
+]
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="app">
+        <ListContacts contacts={contacts} />
       </div>
     );
   }
---
+++ src/ListContacts.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class ListContacts extends React.Component {
+  render() {
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map(contact => (
+          <li key={contact.id} className="contact-list-item">
+            <div className="contact-avatar" style={{
+              backgroundImage: `url(${contact.avatarURL})`
+            }}/>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.email}</p>
+            </div>
+            <button className="contact-remove">
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    )
+  }
+}
+
+export default ListContacts
