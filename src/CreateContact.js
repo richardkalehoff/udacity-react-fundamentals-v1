a2d02bd978c264206e2fd4751fa1ead5103db910
Tyler McGinnis <tylermcginnis33@gmail.com>
2017-04-26 18:20:27 -0600

    Finish CreateContact

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,10 +1,44 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import serializeForm from 'form-serialize'
+import * as ContactsAPI from './utils/ContactsAPI'
+import ImageInput from './ImageInput'
 
 class CreateContact extends React.Component {
+  static propTypes = {
+    onCreateContact: PropTypes.func.isRequired
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+
+    const form = event.target
+    const values = serializeForm(form, { hash: true })
+
+    if (values.name && values.email) {
+      ContactsAPI.create(values).then(contact => {
+        this.props.onCreateContact(contact)
+        form.reset()
+      })
+    } else {
+      window.alert('Please enter both a name and email address')
+    }
+  }
+
   render() {
     return (
       <div>
-        Create Contact
+        <Link className="close-create-contact" to="/">Close</Link>
+
+        <form className="create-contact-form" onSubmit={this.handleSubmit}>
+          <ImageInput className="create-contact-avatar-input" name="avatarURL" maxHeight={64}/>
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="email" placeholder="Email"/>
+            <button>Add Contact</button>
+          </div>
+        </form>
       </div>
     )
   }
